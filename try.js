7645f4c74252cdffac5589e120c0b4d2e691185c
Steven R. Loomis <srloomis@us.ibm.com>
2018-05-15 14:03:58 -0700

    add intermaptxt

--- try.js
+++ try.js
@@ -46,6 +46,49 @@ async function custom(ticket) {
     }, {});
 }
 
+/**
+ * get the latest version of a wiki page
+ * @param {String} name 
+ */
+async function getWiki(name) {
+    return (await dbPromise).get(`select * from wiki where name='${name}' order by version desc limit 1;`);
+}
+
+const InterMapTxt = getWiki('InterMapTxt')
+.then((m) => m.text)
+.then((l) => {
+    let lines = l.split(/[\r\n]+/);
+    lines = lines.slice(lines.indexOf('----')+1);
+    lines = lines.slice(lines.indexOf('{{{')+1);
+    lines = lines.slice(0,lines.lastIndexOf('}}}'));
+    lines = lines.filter((l) => {
+        if (!l.trim()) return false;
+        if (l[0] === '#') return false;
+        return true;
+    });
+    lines = lines.map((l) => {
+        let r = l.trim();
+        // const { a,b } = r.split(/\s*#\s*/);
+        const hash = r.indexOf('#');
+        let title;
+        if(hash>0) {
+            title = r.substring(hash+1).trim();
+            r = r.substring(0,hash).trim();
+        }
+        const [id, url] = r.split(/\s+/);
+        return {id, url, title};
+    });
+    return lines;
+    /*
+      { id: 'RFC',
+    url: 'http://www.ietf.org/rfc/rfc$1.txt',
+    title: 'IETF\'s RFC $1' },
+  { id: 'Acronym',
+    url: 'http://www.acronymfinder.com/af-query.asp?String=exact&Acronym=',
+    title: undefined },
+    */
+});
+
 async function forTracType(type) {
     const jiraType = config.mapTypes[type];
     if(!jiraType) throw Error(`Unknown trac ticket type ${type} (check mapTypes)`);
@@ -63,6 +106,8 @@ async function doit() {
     // console.dir(await nameToIssueType);
     // return;
     // console.dir(await nameToFieldId);
+    console.log(await InterMapTxt);
+    return;
 
     const {count} = await ticketCount;
     console.log(`${count} tickets to process`);
